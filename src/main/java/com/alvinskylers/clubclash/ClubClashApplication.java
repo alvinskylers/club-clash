45e20abab241dd6b09b47a9dddbba932d638398b
alvinskylers <alvinskylers.dev@gmail.com>
2026-02-22 09:41:38 +0800

    initial commit

---
+++ src/main/java/com/alvinskylers/clubclash/ClubClashApplication.java
@@ -0,0 +1,13 @@
+package com.alvinskylers.clubclash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubClashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubClashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alvinskylers/clubclash/ClubClashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvinskylers.clubclash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubClashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
